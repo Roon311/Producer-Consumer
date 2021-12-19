e4e18a2512312c885613cd0e6b291bda7cf1b932
Roon311 <75309751+Roon311@users.noreply.github.com>
2021-12-19 08:52:28 +0200

    Initiation

---
+++ Producer-Consumer/Producer.c
@@ -0,0 +1,20 @@
+//------------------------------------------Includes--------------------------------------------//
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+//----------------------------------------------------------------------------------------------//
+int main()
+{
+    printf("\nHello sakka\n\n");
+    return 0;
+}
---
+++ Producer-Consumer/consumer.c
@@ -0,0 +1,20 @@
+//------------------------------------------Includes--------------------------------------------//
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+//----------------------------------------------------------------------------------------------//
+void main()
+{
+    printf("\nHello world\n\n");
+    return;
+}
