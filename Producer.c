030f568d72b288c7f441e5b60a8e1f616e3271d4
Roon311 <narnar10102001@gmail.com>
2021-12-19 21:02:06 +0200

    shared memory edits in producer .c

--- Producer.c
+++ Producer.c
@@ -13,16 +13,19 @@
 #include <sys/shm.h>
 #include <sys/sem.h>
 //----------------------------------------------------------------------------------------------//
-
-
+#define true 1
+#define false 0
 //------------------------------------------Global Variables------------------------------------//
 
-int shmid;  // shared memory ID
-int sem1 ;  // semaphore 1
-int sem2 ;  // semaphore 2
-
+int shmid; // shared memory ID
+char* shmaddr;// char to know attachement location
+char *ptr;//to divide the mem
+int sem1;  // semaphore 1
+int sem2;  // semaphore 2
+key_t shm_key = 12613;
+const int shm_size = 1024;
+const int buffer_size=10;
 //----------------------------------------------------------------------------------------------//
-
 
 //-------------------------------------- Semaphores ----------------------------------------------//
 /* arg for semctl system calls. */
@@ -105,21 +108,33 @@ void cleanup(int signum)
 }
 //------------------------------------------------------------------------------------------------//
 
-
 //------------------------------------------Producing Function------------------------------------//
 void Produce(int shmid)
 {
-    
+    int next[buffer_size];
+    shmaddr = (char*) shmat (shmid, 0, 0); 
+    if(atoi(shmaddr) == -1)
+    {
+        perror("Error in attach in writer");
+        exit(-1);
+    }
+    else
+    {
+        printf("\nShared memory attached at address %x\n", shmaddr);
+        //strcpy((char*) shmaddr, "Initial string!");
+    }
+    while(true)
+    {
+        printf("\nProducing...\n\n");
+    }
 }
 
-
 //------------------------------------------------------------------------------------------------//
-
 
 //------------------------------------------Main------------------------------------------------//
 void main()
 {
     sem1 = create_sem(12614, 0);
     sem2 = create_sem(12615, 0);
-    shmid = shmget(12613, 256, IPC_CREAT|0644);//creating the shared memory    return 0;
+    shmid = shmget(shm_key, shm_size, IPC_CREAT | 0644); // creating the shared memory
 }
